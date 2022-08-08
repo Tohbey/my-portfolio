b198006d8e5231a1deb02b57702081eaa7c7c105
tobey <fafoworatobi25@gmail.com>
2022-08-08 17:06:57 +0100

    styles update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+  workHistory = ["Apple", "Google","Goldman", "Systemspecs", "BrilloConnect", "Exalt", "SystemSpecs"]
 }
